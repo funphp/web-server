a1a54189904d448518fb47b67bdb4aee1d003e44
Abdullah <ab_duetcse@yahoo.com>
2017-03-06 23:15:39 +0600

    inital commit

---
+++ middleware.js
@@ -0,0 +1,15 @@
+var middleware = {
+    requireAuthentication : function(req, res, next) {
+        console.log('private route');
+        next();
+    },
+    logger: function(req, res, next) {
+        var date = new Date().toString();
+
+        console.log('Request: '+req.method + ' '+ req.originalUrl+ ' at '+date);
+
+        next();
+    }
+}
+
+module.exports = middleware;
---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+var port = 3000;
+
+var middleware = require('./middleware');
+
+app.use(middleware.logger);
+//app.use(middleware.requireAuthentication);
+app.get('/about', middleware.requireAuthentication, function(req, res){
+    res.send('About Page');
+});
+
+app.use(express.static(__dirname+'/public'));
+
+app.listen(port, function(){
+    console.log('Server is running on port '+port);
+})
